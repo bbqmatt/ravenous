0e656427bee4dc6b8b8aea40ae0a70e26e56dd71
Matt <subs@mattgrill.com>
2018-06-26 17:02:44 +1000

    part 4 of the project - still got bugs though

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
+import Yelp from './util/Yelp';
 
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
@@ -16,19 +17,21 @@ const business = {
   reviewCount: 90
 };
 
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-];
 
 class App extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy){
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term,location,sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
   }
 
 
@@ -37,7 +40,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
---
+++ src/util/Yelp.js
@@ -0,0 +1,41 @@
+//client ID : dBDNhT3QrMk0AkD-qa_hhw
+
+//you need the fetch() polyfill package to ensure support for older browers.
+//install by running: npm install whatwg-fetch --save
+
+//also using the CORS anywhere API to get around the CORS issues
+//https://cors-anywhere.herokuapp.com/
+
+const apiKey = 'Q4amXLyjeFZ8wzZK304AX7UIuR1Kw3MqW43NRdbPDSOOC_nGBKso1s8lWZ6BN51jX0WBFyo6FNzmY1qq-nj8nxEEOGm5SEZjYqOA5caRhBLpG95f23ZW7FoB9dcxW3Yx';
+
+const Yelp = {
+  search(term, location, sortBy){
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    {
+      headers :{
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if(jsonResponse.businesses){
+        return jsonResponse.businesses.map(business => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }
+        });
+      }
+    });
+  }
+};
+
+export default Yelp;
